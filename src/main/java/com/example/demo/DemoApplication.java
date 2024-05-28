3142e85610647c127222d1430982248822d76b8c
kimdonguk <donguk@mz.co.kr>
2024-05-28 18:46:29 +0900

    feat: Graal Compiler operation test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws RunnerException {
     SpringApplication.run(DemoApplication.class, args);
-  }
 
+    Options opt = new OptionsBuilder()
+        .include(JavaSimpleStreamBenchmark.class.getSimpleName())
+        .forks(1)
+        .build();
+
+    new Runner(opt).run();
+
+  }
 }
